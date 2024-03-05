2dd0c5ecf5710d8d49a2674bf9c9b3916e6bd864
SpottedLeaf43 <sahda898@outlook.com>
2024-03-05 12:38:21 +0100

    Added Sprint Module

--- src/main/java/com/dev/air/module/impl/movement/SprintMod.java
+++ src/main/java/com/dev/air/module/impl/movement/SprintMod.java
@@ -1,2 +1,22 @@
-package com.dev.air.module.impl.movement;public class SprintMod {
+package com.dev.air.module.impl.movement;
+
+import com.dev.air.event.impl.packet.update.PreMotionEvent;
+import com.dev.air.module.api.Category;
+import com.dev.air.module.api.Module;
+import com.dev.air.module.api.annotation.ModuleInfo;
+import net.lenni0451.asmevents.event.Target;
+
+@ModuleInfo(name = "Sprint", description = "Holds down your sprint key", category = Category.MOVEMENT)
+public class SprintMod extends Module {
+
+    @Target
+    public void onPreMotion(PreMotionEvent event) {
+        mc.gameSettings.keyBindSprint.setPressed(true);
+    }
+
+    @Override
+    public void onDisable() {
+        mc.gameSettings.keyBindSprint.setPressed(false);
+        super.onDisable();
+    }
 }
